403a505ffc0ae14dcb2535c816e60cab247c06c1
Andy Hopwood <andy@andrewhopwood.co.uk>
2020-04-08 22:52:30 +0100

    added some text

--- src/components/HomeDisplay/HomeDisplay.js
+++ src/components/HomeDisplay/HomeDisplay.js
@@ -6,7 +6,9 @@ class HomeDisplay extends React.Component {
   render() {
     return (
       <div id="HomeDisplay">
+        <span className="big-text top-text">Hi, My name is Andy Hopwood and I'm a developer from Oldham in the untied Kingdom</span>
         <img alt="me at keyboard" className="big-home" src={me} />
+        <span className="big-text">I make web applications, websites and business needs automation solutions using various technologies</span>
       </div>
     );
   }
